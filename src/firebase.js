348b5999e80c318b9b870fbaf54214b71d9b7196
iloche <112648454+iloche@users.noreply.github.com>
2024-06-06 20:55:56 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBc0AY8dtuottidlzx4yBVLsjKcuANMtuQ",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
