c4b6d398a63f70c55bfcbc1bd920308dd69e00a3
iloche <ilayda.t0912@gmail.com>
2024-06-16 22:03:21 +0200

    Bonus points Frog

--- src/pages/game/game.jsx
+++ src/pages/game/game.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
-import { auth } from "../../firebase";
 import style from './game.module.scss';
+import { auth } from "../../firebase";
 import quizData from '../../data/quizz.json';
 
-const categories = ["Animaux", "Culture","Dessins animés", "Géographie", "Histoire", "Littérature" , "Monde", "Musique", "Sports"];
+const categories = ["Animaux", "Culture", "Dessins animés", "Géographie", "Histoire", "Littérature", "Monde", "Musique", "Sports"];
 const difficultyLevels = ["Débutant", "Confirmé", "Expert"];
 
 const Game = () => {
@@ -25,6 +25,8 @@ const Game = () => {
     const [showExpertUnlockedMessage, setShowExpertUnlockedMessage] = useState(false);
     const [userId, setUserId] = useState(null);
     const [showButtons, setShowButtons] = useState(false);
+    const [expertUnlockedMessageShown, setExpertUnlockedMessageShown] = useState(false);
+    
 
     useEffect(() => {
         const user = auth.currentUser;
@@ -55,6 +57,7 @@ const Game = () => {
         if (showExpertUnlockedMessage) {
             const timer = setTimeout(() => {
                 setShowExpertUnlockedMessage(false);
+                setExpertUnlockedMessageShown(true);
             }, 3000);
             return () => clearTimeout(timer);
         }
@@ -101,13 +104,19 @@ const Game = () => {
             const isCorrect = selectedChoice === currentQuestion.reponse;
             setIsAnswerCorrect(isCorrect);
             setSelectedAnswer(selectedChoice);
-
+    
             if (isCorrect) {
-                setScore(score + 1);
+                const newScore = score + 1;
+                setScore(newScore);
                 setShowAnecdote(true);
-                setCanRetry(false); // Désactive le réessai en cas de réponse correcte
+                setCanRetry(false);
+    
+                if (newScore >= 100 && !expertUnlockedMessageShown) {
+                    setExpertUnlocked(true);
+                    setShowExpertUnlockedMessage(true);
+                }
             } else {
-                setCanRetry(true); // Active le réessai en cas de réponse incorrecte
+                setCanRetry(true);
             }
             setAnswered(true);
         }
@@ -122,10 +131,6 @@ const Game = () => {
         setCanRetry(false); // Réinitialise l'état de réessai
 
         if (currentQuestionIndex === totalQuestions - 1) {
-            if (selectedDifficulty === "Débutant" || selectedDifficulty === "Confirmé") {
-                setExpertUnlocked(true);
-                setShowExpertUnlockedMessage(true);
-            }
             setShowCongratulations(true);
         }
     };
@@ -155,44 +160,99 @@ const Game = () => {
     };  
 
     const handleBackToCategorySelection = () => {
-    setShowCategoryPopup(true);
-    setShowDifficultyPopup(false);
-    setShowQuestionPopup(false);
-    setCurrentQuestionIndex(0); // Réinitialise l'index des questions, mais garde le score
-    setShowAnecdote(false);
-    setSelectedAnswer(null);
-    setIsAnswerCorrect(null);
-    setShowCongratulations(false);
-    setAnswered(false);
-    setShowButtons(false);
-};
-  
+        setShowCategoryPopup(true);
+        setShowDifficultyPopup(false);
+        setShowQuestionPopup(false);
+        setCurrentQuestionIndex(0); // Réinitialise l'index des questions, mais garde le score
+        setShowAnecdote(false);
+        setSelectedAnswer(null);
+        setIsAnswerCorrect(null);
+        setShowCongratulations(false);
+        setAnswered(false);
+        setShowButtons(false);
+    };
+
     const [musicPlaying, setMusicPlaying] = useState(false);
+    const [currentAudio, setCurrentAudio] = useState(null);
+
+    const audioFiles = [
+        '../../../doc/bossin over.mp3',
+        '../../../doc/happy tears.mp3',
+        '../../../doc/nobonoko - My Heart is a Machine.mp3',
+        '../../../doc/Orange Lounge - MOBO MOGA (HQ).mp3',
+        '../../../doc/Stargaze.mp3'
+    ];
+
+    const [buttonImage, setButtonImage] = useState('../../../doc/volume-silent-line-icon.png');
 
     const toggleMusic = () => {
+        if (!musicPlaying) {
+            const randomIndex = Math.floor(Math.random() * audioFiles.length);
+            setCurrentAudio(audioFiles[randomIndex]);
+            setButtonImage('../../../doc/volume-medium-line-icon.png'); 
+        } else {
+            setCurrentAudio(null);
+            setButtonImage('../../../doc/volume-silent-line-icon.png'); 
+        }
         setMusicPlaying(prevState => !prevState);
     };
-    
+
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [visible, setVisible] = useState(false); // Définissez initialement à false
+
+    useEffect(() => {
+        const moveFrog = () => {
+            const windowWidth = window.innerWidth;
+            const windowHeight = window.innerHeight;
+            const frogWidth = 40; // Ajustez cette valeur selon la taille de votre gif
+            const frogHeight = 40; // Ajustez cette valeur selon la taille de votre gif
+
+            const x = Math.random() * (windowWidth - frogWidth);
+            const y = Math.random() * (windowHeight - frogHeight);
+
+            setPosition({ x, y });
+            setVisible(true);
+
+            const randomDisappearTime = Math.random() * (60000 - 15000) + 15000; // entre 15 et 60 secondes
+            setTimeout(() => {
+                setVisible(false);
+            }, randomDisappearTime);
+        };
+
+        if (showQuestionPopup) {
+            const delay = Math.random() * 15000; // entre 1 et 15 secondes
+            setTimeout(moveFrog, delay);
+        } else {
+            setVisible(false);
+        }
+    }, [showQuestionPopup]);
+
+    const handleClick = () => {
+        const newScore = score + 1;
+        setScore(newScore);
+        setVisible(false);
+    };
+
     return (
         <main className={style['main-jeu']}>
             <div className={style['top-content']}>
                 <div className={clsx(style['game-box'], { [style['shifted']]: showCategoryPopup || showDifficultyPopup || showQuestionPopup })}>
-                {showButtons && (
-                    <div className={style.buttons}>
-                        <button title='Lancer la musique' className={style.button} onClick={toggleMusic}>
-                            <img className={style.music} src='../../../doc/music.png' />
-                        </button>
-                        <button title='Relancer le niveau' className={style.button} onClick={handleRestartButtonClick}>
-                            <img className={style.refresh} src='../../../doc/refresh.png' />
-                        </button>
-                        <button title='Retour' className={style.button} onClick={handleQuitGame}>
-                            <img className={style.return} src='../../../doc/return.png' />
-                        </button>
-                    </div>
-                )}
-                {musicPlaying && (
-                        <audio autoPlay loop volume={50}>
-                            <source src="../../../doc/Joel McNeely-Tinkering.mp3" type="audio/mp3" />
+                    {showButtons && (
+                        <div className={style.buttons}>
+                            <button title='Lancer la musique' className={style.button} onClick={toggleMusic}>
+                                <img className={style.music} src={buttonImage} />
+                            </button>
+                            <button title='Relancer le niveau' className={style.button} onClick={handleRestartButtonClick}>
+                                <img className={style.refresh} src='../../../doc/refresh.png' />
+                            </button>
+                            <button title='Retour' className={style.button} onClick={handleQuitGame}>
+                                <img className={style.return} src='../../../doc/return.png' />
+                            </button>
+                        </div>
+                    )}
+                   {musicPlaying && currentAudio && (
+                        <audio autoPlay loop>
+                            <source src={currentAudio} type="audio/mp3" />
                         </audio>
                     )}
                     {showCategoryPopup ? (
@@ -215,9 +275,15 @@ const Game = () => {
                                 <div className={style["difficulty-box"]}>
                                     {difficultyLevels.map((difficulty, index) => (
                                         <div className={style.level} key={index} onClick={() => handleDifficultyClick(difficulty)}>
-                                            <h2>{difficulty}</h2>
+                                            <h2 title={difficulty === "Expert" && !expertUnlocked ? "Obtiens un score total de 100 points pour débloquer le niveau" : ""}>
+                                                {difficulty}
+                                            </h2>
                                             {difficulty === "Expert" && !expertUnlocked && (
-                                                <img className={style.lock} src="../../../doc/locked.png" alt="Cadenas" />
+                                                <img
+                                                    className={style.lock}
+                                                    src="../../../doc/locked.png"
+                                                    alt="Cadenas"
+                                                />
                                             )}
                                         </div>
                                     ))}
@@ -226,8 +292,9 @@ const Game = () => {
                         </div>
                     ) : showQuestionPopup ? (
                         <div className={clsx("question-title", style.popup)}>
-                                                        <div className={style["question-tag-level"]}>
-                                <h2>Catégorie: {selectedCategory} <span>Difficulté: {selectedDifficulty}</span></h2>
+                            <div className={style["question-tag-level"]}>
+                                <h2 className={style.tagName}>Catégorie: {selectedCategory}</h2>
+                                <h2 className={style.lvlName}>Difficulté: {selectedDifficulty}</h2>
                             </div>
                             <div className={style.question}>
                                 {currentQuestion && (
@@ -251,7 +318,7 @@ const Game = () => {
                                 )}
                                 {showAnecdote && isAnswerCorrect && (
                                     <div>
-                                        <p>{currentQuestion.anecdote}</p>
+                                        <p className={style.anecdote}>{currentQuestion.anecdote}</p>
                                         <button title='Suivant' className={style.start} onClick={handleNextQuestionClick}><img className={style.arrow} src='../../../doc/arrow.png' /></button>
                                     </div>
                                 )}
@@ -262,11 +329,6 @@ const Game = () => {
                             <div className={style.score}>
                                 Score total: {score}
                             </div>
-                            {showExpertUnlockedMessage && (
-                                <div className={style.unlocked}>
-                                    <h2>Niveau Expert Débloqué!</h2>
-                                </div>
-                            )}
                         </div>
                     ) : (
                         <>
@@ -274,21 +336,42 @@ const Game = () => {
                             <button className={style.start} onClick={handleStartButtonClick}>Commencez !</button>
                         </>
                     )}
-
                     {showCongratulations && (
-                       <>
-                       <div className={style.congratulations}>
-                           <h2>Félicitations!</h2>
-                           <p>Vous avez terminé le niveau avec succès.</p>
-                           <img className={style.congrats} src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnFqYnpqM3A4aGJ4Z3N3azZqOXFvb3p6dG9wMWR2ZzV2M3p2dzZ2cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/Vf8UeyP351aaaVj6wR/giphy.gif" alt="Félicitations" />
-                       </div>
-                       <button className={style.start} onClick={handleBackToCategorySelection}>Retour</button>
-                       </>
+                        <>
+                            <div className={style.congratulations}>
+                                <h2>Félicitations!</h2>
+                                <p>Vous avez terminé le niveau avec succès.</p>
+                                <img className={style['corner-image']} src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHZ6ZzdpbTlob2hvMzV6NHZ6enJ1NHl6emplY3lmNG4yN3dkMXp4MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/ZVZTD5kaMZ2d7EEfRn/giphy.gif" alt="Happy Frog" />
+                            </div>
+                            <button className={style.start} onClick={handleBackToCategorySelection}>Retour</button>
+                        </>
+                    )}
+                    {showExpertUnlockedMessage && (
+                        <div className={style.notification}>
+                            <h2>Niveau Expert Débloqué!</h2>
+                            <img className={style.congrats} src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnFqYnpqM3A4aGJ4Z3N3azZqOXFvb3p6dG9wMWR2ZzV2M3p2dzZ2cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/Vf8UeyP351aaaVj6wR/giphy.gif" alt="Félicitations" />
+                        </div>
                     )}
                 </div>
             </div>
+            {visible && (
+                <img
+                    src='https://media1.giphy.com/media/l9bpYbmiaAtwJ7HK09/200.webp?cid=ecf05e47li6m59xuqyfwqlbu36t6q9icqxlk8qpmywyva6wh&ep=v1_gifs_related&rid=200.webp&ct=s'
+                    alt="Grenouille"
+                    style={{
+                        position: 'absolute',
+                        left: `${position.x}px`,
+                        top: `${position.y}px`,
+                        width: '50px',
+                        height: '50px',
+                        cursor: 'pointer',
+                    }}
+                    onClick={handleClick}
+                />
+            )}
         </main>
     );
 };
 
 export default Game;
+
